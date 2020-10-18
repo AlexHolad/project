85aff375a73efc28e650e5de60fa7fd5f696ec0c
Alexej Holad <alexej.holad@gmail.com>
2020-10-18 15:25:19 +0200

    objects for in methods of objects

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 "use strict";
 
-function learnJS (lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colours: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log('Test');
+    }
+};
 
-function done() {
-    console.log('Я прошел этот урок');
-}
-learnJS('JavaScript', done);
+options.makeTest ();
 
+// const {border, bg} = options.colours;
+// console.log(border);
+
+
+// console.log(Object.keys(options).length); // Показывает количество свойств в объекте
+
+// console.log(options.name); - показывает значение свойства
+
+// delete options.name; - удаляет свойство в объекте
+
+// console.log(options['colours']['border']); // выводит свойство вложенного объекта
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`); 
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+    
+// }
+
+// console.log(counter);
