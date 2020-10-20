4a83dbfe6806f5ebe034736b359a70058904c6c3
Alexej Holad <alexej.holad@gmail.com>
2020-10-20 13:58:52 +0200

    clone object and array

--- script.js
+++ script.js
@@ -1,36 +1,99 @@
 "use strict";
 
-const arr = [1, 10, 8, 4, 5];
-arr.sort(compareNum);
-console.log(arr);
+let a = 5,
+    b = a;
 
-function compareNum (a, b) {
-    return a - b;
+    b = b + 5;
+
+    console.log(b, a);
+
+let obj = {
+    a : 5,
+    b : 10
+};
+
+// const copy = obj; // Ссылку на объект
+
+// copy.a = 10;
+
+// console.log(copy, obj);
+
+
+function copy(mainObj) {                   //  Создание поверхносной копии объекта
+    let objCopy = {};                      //
+                                           //
+    let key;                               //
+    for (key in mainObj) {                 //
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
 }
-// arr[99] = 0;
-// console.log(arr.length); // кол-во элементов массива + 1
-// console.log(arr);
-
-arr.forEach(function(item, i, arr) {
-    console.log(`${i}: ${item} внутри массива ${arr}`)
-}); 
-
-//arr.pop();  // - удаляет последний элемент массива
 
 
-// arr.push(10); // - добавляет в конец массива 10
-// console.log(arr);
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+const newNumbers = copy(numbers);
+
+newNumbers.c.y = 10;
+console.log(newNumbers, numbers);
+console.log(numbers);
+
+// Встраивание объекта в объект, создание поверхносной копии
+
+const add = {
+    d: 17,
+    e: 22
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a','b','c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'dfxcgvhb';
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blog = ['livejournal', 'worldpress', 'blogger'],
+      internet = [...video, ...blog, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-for (let value of arr) {
-    console.log(value);
-}
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a','b'];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+
+console.log(newObj);
 
 
-const str = prompt("", "");  // - вписать свойство в строку
-const products = str.split(", "); // - строку разбить в массив
-products.sort(); // - отсортировать в алфавитном порядке как строки
-console.log(products.join(';')); // - объединить в строку
+
